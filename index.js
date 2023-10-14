4e8534aec112bc925d85e788758cf90228db63a1
Agbadudu Oghenerukevwe <90446020+Dev-rukky@users.noreply.github.com>
2023-10-13 19:10:29 -0700

    done

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSettings = {
     databaseURL: "https://realtime-database-88260-default-rtdb.firebaseio.com/"
@@ -13,7 +13,7 @@ const shoppingListEl = document.getElementById("shopping-list");
 const addButtonEl = document.getElementById("add-button");
 
 addButtonEl.addEventListener("click", () => {
-   const inputValue = inputFieldEl.value;
+    const inputValue = inputFieldEl.value;
 
     push(shoppingListDB, inputValue);
 
@@ -21,15 +21,20 @@ addButtonEl.addEventListener("click", () => {
 });
 
 onValue(shoppingListDB, (snapshot) => {
-    const itemsArray = Object.entries(snapshot.val());
-    
-     clearShoppingListEl();
-    for (let i = 0; i < itemsArray.length; i++) {
-        let currentItem = itemsArray[i]
-        let currentItemID = currentItem[0]
-        let currentItemValue = currentItemID
-        appendItemToShoppingListEl(currentItem);
+    if (snapshot.exists()) {
+        const itemsArray = Object.entries(snapshot.val());
+
+        clearShoppingListEl();
+        for (let i = 0; i < itemsArray.length; i++) {
+            let currentItem = itemsArray[i]
+            let currentItemID = currentItem[0]
+            let currentItemValue = currentItem[1]
+            appendItemToShoppingListEl(currentItem);
+        }
+    }else {
+        shoppingListEl.innerHTML = "No items here.... yet"
     }
+    
 });
 
 const clearInputFieldEl = () => {
@@ -43,8 +48,17 @@ const appendItemToShoppingListEl = (item) => {
     newEl.textContent = itemValue
 
     shoppingListEl.append(newEl)
-  
+
+    newEl.addEventListener("click", () => {
+        deleteItemFromFirebase(itemID)
+    })
+
 };
+
+const deleteItemFromFirebase = (itemKey) => {
+    let exactLocationOfItemInDB = ref(database, `shoppingList/${itemKey}`)
+    remove(exactLocationOfItemInDB)
+}
 
 const clearShoppingListEl = () => {
     shoppingListEl.innerHTML = "";
